5a4b5008b475bb9d0409fa3eaf43ffb804d19e6d
Douglas Hewitt <madebydouglas@gmail.com>
2019-11-05 13:47:17 -0800

    run pod install and update sample
    pod install using newer version of cocoapods, also run "migrate to swift 5" for sample project

--- RichEditorViewSample/Pods/Target Support Files/Pods-RichEditorViewSample/Pods-RichEditorViewSample-umbrella.h
+++ RichEditorViewSample/Pods/Target Support Files/Pods-RichEditorViewSample/Pods-RichEditorViewSample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double RichEditorViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char RichEditorViewVersionString[];
+FOUNDATION_EXPORT double Pods_RichEditorViewSampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RichEditorViewSampleVersionString[];
 
